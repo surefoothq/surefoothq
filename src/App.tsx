1c77fc885e336b6c457cd6f1564e79bb703b0041
Sadiq Salau <sadiqsalau888@gmail.com>
2025-12-28 09:03:46 +0100

    feat: Added under construction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import Logo from "./assets/logo.svg";
+
 function App() {
-  return <></>;
+  return (
+    <main class="flex flex-col bg-[#fbcbbb] h-dvh">
+      <div class="shrink-0 py-10 flex flex-col gap-4">
+        <h1 class="text-center text-4xl lg:text-6xl font-boldonse">
+          Surefoot HQ
+        </h1>
+        <p class="text-center">Under Construction...</p>
+      </div>
+      <div class="grow relative">
+        <img
+          src={Logo}
+          alt="Surefoot Logo"
+          class="absolute size-full object-contain object-bottom"
+        />
+      </div>
+    </main>
+  );
 }
 
 export default App;
