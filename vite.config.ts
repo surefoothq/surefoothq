b2a5281ccb2d55ebd71213d7da8dffcc6d057d04
Sadiq Salau <sadiqsalau888@gmail.com>
2025-12-28 08:38:43 +0100

    feat: Added vite imagetools

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
+import { imagetools } from "vite-imagetools";
 
 export default defineConfig({
-  plugins: [solid(), tailwindcss()],
+  plugins: [solid(), tailwindcss(), imagetools()],
 });
